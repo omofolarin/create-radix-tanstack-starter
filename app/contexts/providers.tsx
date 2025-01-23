e7073b4625c25a555831a44f0855164f9efa8a0a
omofolarin <folarinshonibare@yahoo.com>
2025-01-23 21:17:36 +0100

    fixed radix theme provider

--- app/contexts/providers.tsx
+++ app/contexts/providers.tsx
@@ -1,27 +1,46 @@
-import React from 'react';
+import { ThemeProvider as NextThemesProvider, ThemeProviderProps } from 'next-themes';
+import * as React from 'react';
+import * as Toast from '@radix-ui/react-toast';
 import { Theme, ThemePanel } from '@radix-ui/themes';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { ThemeProvider } from 'next-themes';
-import * as Toast from '@radix-ui/react-toast';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+import { ToastsProvider } from '@/components/notifications';
 
-const queryClient = new QueryClient();
-
-export function RootProviders({ children }: { children: React.ReactNode }) {
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
-    <ThemeProvider attribute="class">
-      <QueryClientProvider client={queryClient}>
-        <Theme accentColor="blue" grayColor="slate" radius="medium">
-          {children}
-        </Theme>
-        <ReactQueryDevtools initialIsOpen={false} />
-        <ThemePanel defaultOpen={false} />
-        <TanStackRouterDevtools position="bottom-left" />
-
-        <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" />
-        <Toast.Viewport className="fixed right-0 bottom-0 z-[2147483647] m-0 flex w-[390px] max-w-[100vw] list-none flex-col gap-2.5 p-[var(--viewport-padding)] [--viewport-padding:_25px] outline-none" />
-      </QueryClientProvider>
-    </ThemeProvider>
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="light"
+      enableSystem
+      disableTransitionOnChange
+      {...props}
+    >
+      <Theme accentColor="blue" radius="medium" scaling="100%">
+        {children}
+      </Theme>
+    </NextThemesProvider>
   );
 }
+
+interface RootProvidersProps {
+  children: React.ReactNode;
+}
+
+export const queryClient = new QueryClient();
+export const RootProviders = (props: RootProvidersProps) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <ToastsProvider position="topRight">
+          {props.children}
+          <ThemePanel defaultOpen={false} />
+          <TanStackRouterDevtools position="bottom-left" />
+
+          <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" />
+          <Toast.Viewport className="fixed right-0 bottom-0 z-[2147483647] m-0 flex w-[390px] max-w-[100vw] list-none flex-col gap-2.5 p-[var(--viewport-padding)] [--viewport-padding:_25px] outline-none" />
+        </ToastsProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+};
